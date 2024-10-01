a265b76905b058182ee5543dd9fcc0d48908197a
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-10-01 22:06:32 +0200

    Initial commit

---
+++ src/test/java/com/openclassrooms/chatop/ChatopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.chatop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
