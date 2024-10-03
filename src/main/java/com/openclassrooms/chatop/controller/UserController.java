085e160eb5427546f0f0c10f0294e74b25fa7fed
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-10-03 15:50:16 +0200

    feat:add API endpoints (not yet implemented)

--- src/main/java/com/openclassrooms/chatop/controller/UserController.java
+++ src/main/java/com/openclassrooms/chatop/controller/UserController.java
@@ -1,7 +1,54 @@
 package com.openclassrooms.chatop.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.openclassrooms.chatop.dto.UserDto;
+import com.openclassrooms.chatop.mapper.UserMapper;
+import com.openclassrooms.chatop.model.User;
+import com.openclassrooms.chatop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/auth")
 public class UserController {
+
+    private final UserService userService;
+
+    private final UserMapper userMapper = UserMapper.INSTANCE;
+
+    public UserController(@Autowired final UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
+    public String register(@RequestBody final User user) {
+        return "TODO : register and generate token";
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
+    public String login() {
+        return "TODO : login";
+    }
+
+    @GetMapping("/me")
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
+    public String getUserInfo(Principal principal) {
+        return "TODO";
+    }
+
+    @GetMapping("/user/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDto getUser(@PathVariable long id) {
+        Optional<User> foundUser = userService.findUserById(id);
+        if(foundUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        return userMapper.userToUserDTO(foundUser.get());
+    }
 }
