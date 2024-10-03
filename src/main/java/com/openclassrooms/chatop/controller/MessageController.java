e9827d32dbd474da410a58bd66be89f08a1c2097
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-10-03 15:49:13 +0200

    feat:add messages api endpoint (not yet implemented)

--- src/main/java/com/openclassrooms/chatop/controller/MessageController.java
+++ src/main/java/com/openclassrooms/chatop/controller/MessageController.java
@@ -1,7 +1,16 @@
 package com.openclassrooms.chatop.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.openclassrooms.chatop.model.Message;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/messages")
 public class MessageController {
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
+    public String createMessage(@RequestBody Message message) {
+        return "TODO : create message";
+    }
 }
