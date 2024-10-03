580ab982852bac0168d9ab4b970c40a978950137
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-10-03 16:47:18 +0200

    feat:add validation for request body for user registration

--- src/main/java/com/openclassrooms/chatop/controller/UserController.java
+++ src/main/java/com/openclassrooms/chatop/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.openclassrooms.chatop.controller;
 
+import com.openclassrooms.chatop.dto.JwtTokenDto;
+import com.openclassrooms.chatop.dto.RegisterUserDto;
 import com.openclassrooms.chatop.dto.UserDto;
 import com.openclassrooms.chatop.mapper.UserMapper;
 import com.openclassrooms.chatop.model.User;
+import com.openclassrooms.chatop.repository.UserRepository;
 import com.openclassrooms.chatop.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @RestController
@@ -26,8 +31,16 @@ public class UserController {
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-    public String register(@RequestBody final User user) {
-        return "TODO : register and generate token";
+    public JwtTokenDto register(@Valid @RequestBody final RegisterUserDto registerUserDto) {
+        User registerUser = new User();
+        registerUser.setEmail(registerUserDto.getEmail());
+        registerUser.setPassword(registerUserDto.getPassword());
+        registerUser.setName(registerUserDto.getName());
+        registerUser.setCreatedAt(LocalDateTime.now());
+        registerUser.setUpdatedAt(LocalDateTime.now());
+
+        User user = userService.registerUser(registerUser);
+        return new JwtTokenDto("jwt");
     }
 
     @PostMapping("/login")
