6c908880cfb1b961fcafb19839d3a30c7ed84385
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-10-05 10:19:39 +0200

    feat:add different request dtos for rental creation and update

--- src/main/java/com/openclassrooms/chatop/dto/CreateRentalRequestDto.java
+++ src/main/java/com/openclassrooms/chatop/dto/CreateRentalRequestDto.java
@@ -2,20 +2,19 @@ package com.openclassrooms.chatop.dto;
 
 import com.openclassrooms.chatop.validator.ImageFile;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import org.hibernate.validator.constraints.URL;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
-public class RentalRequestDto {
+public class CreateRentalRequestDto implements RentalActionRequestDto {
     @NotBlank(message = "The name is required")
     private String name;
     @NotNull(message = "Rental surface is required")
     private float surface;
     @NotNull(message = "Rental price is required")
     private float price;
+    @NotNull(message = "Image file is required")
     @ImageFile
     private MultipartFile picture;
     @NotBlank(message = "The description is required")
---
+++ src/main/java/com/openclassrooms/chatop/dto/UpdateRentalRequestDto.java
@@ -0,0 +1,21 @@
+package com.openclassrooms.chatop.dto;
+
+import com.openclassrooms.chatop.validator.ImageFile;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class UpdateRentalRequestDto implements RentalActionRequestDto {
+    @NotBlank(message = "The name is required")
+    private String name;
+    @NotNull(message = "Rental surface is required")
+    private float surface;
+    @NotNull(message = "Rental price is required")
+    private float price;
+    @ImageFile
+    private MultipartFile picture;
+    @NotBlank(message = "The description is required")
+    private String description;
+}
