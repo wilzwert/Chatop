749e9636d44e30b4bf9f8661ca5c54de1d2072f2
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-10-05 10:23:32 +0200

    feat:add different request dtos for rental creation and update

---
+++ src/main/java/com/openclassrooms/chatop/dto/RentalActionRequestDto.java
@@ -0,0 +1,4 @@
+package com.openclassrooms.chatop.dto;
+
+public interface RentalActionRequestDto {
+}
--- src/main/java/com/openclassrooms/chatop/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/openclassrooms/chatop/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.openclassrooms.chatop.exceptions;
 
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
@@ -45,5 +47,14 @@ public class GlobalExceptionHandler {
         errorDTO.setStatus(String.valueOf(ex.getStatusCode().value()));
         errorDTO.setTime(new Date().toString());
         return new ResponseEntity<ErrorDTO>(errorDTO, ex.getStatusCode());
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorDTO> generateMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.setStatus(String.valueOf(HttpStatus.PAYLOAD_TOO_LARGE.value()));
+        errorDTO.setMessage("Payload too large");
+        errorDTO.setTime(new Date().toString());
+        return new ResponseEntity<ErrorDTO>(errorDTO, HttpStatus.PAYLOAD_TOO_LARGE);
     }
 }
