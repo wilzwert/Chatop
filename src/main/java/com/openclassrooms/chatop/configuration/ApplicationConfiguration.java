499ffd27426d52724e213a7ce80716217a8aa034
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-10-05 12:37:02 +0200

    chore:code cleanup

--- src/main/java/com/openclassrooms/chatop/configuration/ApplicationConfiguration.java
+++ src/main/java/com/openclassrooms/chatop/configuration/ApplicationConfiguration.java
@@ -2,7 +2,6 @@ package com.openclassrooms.chatop.configuration;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
