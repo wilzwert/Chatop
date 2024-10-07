e082cc8a2ec7c278f1099d57b3c94a77f4e28556
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-10-07 09:44:03 +0200

    feat: add support for external configuration in .env for server, db and jwt token properties

--- src/main/java/com/openclassrooms/chatop/configuration/ApplicationConfiguration.java
+++ src/main/java/com/openclassrooms/chatop/configuration/ApplicationConfiguration.java
@@ -2,23 +2,21 @@ package com.openclassrooms.chatop.configuration;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+
 @Configuration
-@Getter
-public class ApplicationConfiguration implements WebMvcConfigurer {
+@PropertySource("file:./.env")
+public class ApplicationConfiguration {
 
-    private final StorageProperties storageProperties;
-
-    public ApplicationConfiguration(@Autowired StorageProperties storageProperties) {
-        this.storageProperties = storageProperties;
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/"+storageProperties.getUploadDir()+"/**")
-                .addResourceLocations("file:" + System.getProperty("user.dir") + "/"+storageProperties.getUploadDir()+"/");
-    }
 }
