0043c0728a99fd2fdebf2c6d27d945e469800527
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-10-07 09:42:09 +0200

    refactor: separate config files for Application and Storage

---
+++ src/main/java/com/openclassrooms/chatop/configuration/StorageConfiguration.java
@@ -0,0 +1,25 @@
+package com.openclassrooms.chatop.configuration;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Getter
+public class StorageConfiguration implements WebMvcConfigurer {
+
+    private final StorageProperties storageProperties;
+
+    public StorageConfiguration(@Autowired StorageProperties storageProperties) {
+        this.storageProperties = storageProperties;
+    }
+
+    // set resource handler for uploaded files
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/"+storageProperties.getUploadDir()+"/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/"+storageProperties.getUploadDir()+"/");
+    }
+}
