37c50af0095cdd4a9b2b9c74d14d01e0c87326c3
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2024-10-08 11:17:08 +0200

    chore: code cleanup

--- src/main/java/com/openclassrooms/chatop/service/CustomAclService.java
+++ src/main/java/com/openclassrooms/chatop/service/CustomAclService.java
@@ -7,7 +7,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.acls.domain.BasePermission;
 import org.springframework.security.acls.domain.ObjectIdentityImpl;
 import org.springframework.security.acls.domain.PrincipalSid;
 import org.springframework.security.acls.domain.SidRetrievalStrategyImpl;
@@ -15,17 +14,12 @@ import org.springframework.security.acls.model.*;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.attribute.UserPrincipal;
-import java.security.Principal;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class CustomAclService implements AclService {
 
-    private final MutableAclService aclService;
+    private final CustomAclPermissionService customAclPermissionService;
 
     private static final Logger logger = LogManager.getLogger(CustomAclService.class);
 
@@ -37,13 +31,10 @@ public class CustomAclService implements AclService {
             "INNER JOIN acl_sid ON acl_entry.sid = acl_sid.id " +
             "WHERE acl_sid.sid = ?";
 
-    // default owner permissions for a domain object
-    private static final List<Permission> defaultOwnerPermissions = Arrays.asList(BasePermission.READ, BasePermission.WRITE, BasePermission.DELETE);
-
     private JdbcTemplate jdbcTemplate;
 
-    public CustomAclService(MutableAclService aclService, JdbcTemplate jdbcTemplate) {
-        this.aclService = aclService;
+    public CustomAclService(CustomAclPermissionService customAclPermissionService, JdbcTemplate jdbcTemplate) {
+        this.customAclPermissionService = customAclPermissionService;
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -51,41 +42,26 @@ public class CustomAclService implements AclService {
         logger.info("Creating owner acl for rental {}", rental.getId());
         try {
             ObjectIdentity oid = new ObjectIdentityImpl(Rental.class, rental.getId());
-            List<Sid> sids = new SidRetrievalStrategyImpl().getSids(SecurityContextHolder.getContext().getAuthentication());
-            MutableAcl acl = aclService.createAcl(oid);
-            logger.info("Granting owner permissions for rental  {}, oid {}, sids {}", rental.getId(), oid, sids);
-            grantOwnerPermissions(acl, sids);
+            List<Sid> sids = getContextSid();
+            customAclPermissionService.grantOwnerPermissions(oid, sids);
         }
         catch(Exception e) {
-            logger.error("Error creating owner acl {}", e.getMessage());
-            throw e;
+            logger.error("Error creating owner acl for rental {}", rental.getId(), e);
         }
     }
 
-    public void grantOwnerPermissions(MutableAcl acl, List<Sid> sids) {
-        grantOwnerPermissions(acl, sids, defaultOwnerPermissions);
-    }
-
-    @Transactional
-    public void grantOwnerPermissions(MutableAcl acl, List<Sid> sids, List<Permission> permissions) {
-        sids.forEach((Sid sid) ->
-                permissions.forEach((Permission permission) -> {
-                    logger.info("Creating owner acl for Sid {} and permission {}", sid, permission);
-                    acl.insertAce(acl.getEntries().size(), permission, sid, true);
-                })
-        );
-        aclService.updateAcl(acl);
-    }
 
     @Override
+    @Transactional
     public void removeAllPermissions(Rental rental) {
+        logger.info("Removing all permissions for rental {}", rental.getId());
         ObjectIdentity oid = new ObjectIdentityImpl(Rental.class, rental.getId());
-        logger.info("Removing all permissions for rental {}, oid {}", rental.getId(), oid);
-        aclService.deleteAcl(oid, true);
+        customAclPermissionService.removeAllPermissions(oid);
         logger.info("All permissions removed.");
     }
 
     @Override
+    @Transactional
     public void removeAllPermissions(User user) {
         PrincipalSid sid = new PrincipalSid(user.getEmail());
 
@@ -103,37 +79,14 @@ public class CustomAclService implements AclService {
 
         if(!objectIdentities.isEmpty()) {
             try {
-                // get acls for found object identities, filtered by sid
-                Map<ObjectIdentity, Acl> acls = aclService.readAclsById(objectIdentities, List.of(sid));
-                logger.info("Remove all permissions, found acls {}", acls);
-                for (Acl acl : acls.values()) {
-                    MutableAcl mutableAcl = (MutableAcl) acl;
-                    for (int i = 0; i < acl.getEntries().size(); i++) {
-                        if (acl.getEntries().get(i).getSid().equals(sid)) {
-                            logger.info("Remove all permissions : remove entry {}", acl.getEntries().get(i));
-                            mutableAcl.deleteAce(i);
-                            // deletion removes index, we have to manually adjust
-                            i--;
-                        }
-                    }
-
-                    if (mutableAcl.getEntries().isEmpty()) {
-                        logger.info("Remove all permissions : delete acl {}", mutableAcl);
-                        aclService.deleteAcl(acl.getObjectIdentity(), true);
-                    } else {
-                        logger.info("Remove all permissions : acl entries not empty {}", mutableAcl.getEntries());
-                        aclService.updateAcl(mutableAcl);
-                    }
-                }
-            } catch (NotFoundException e) {
-                logger.info("Remove all permissions : no acls found for {}", objectIdentities);
+                customAclPermissionService.removeAllPermissions(objectIdentities, List.of(sid));
             } catch (Exception e) {
                 logger.error("Remove all permissions : error encountered {}", e.getMessage());
             }
         }
     }
 
-    @Override
+    @Transactional
     public void removeUser(User user) {
         removeAllPermissions(user);
         PrincipalSid sid = new PrincipalSid(user.getEmail());
@@ -143,5 +96,9 @@ public class CustomAclService implements AclService {
         } catch (DataAccessException e) {
             logger.error("Remove user  :error encountered while deleting sid {}", e.getMessage());
         }
+    }
+
+    private List<Sid> getContextSid() {
+        return new SidRetrievalStrategyImpl().getSids(SecurityContextHolder.getContext().getAuthentication());
     }
 }
